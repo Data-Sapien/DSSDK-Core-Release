d5def4c08f66a076c47fab6343eb748fd2bb0e8e
Arda Dogantemur <ardadogantemur@gmail.com>
2025-11-01 01:07:48 +0300

    new version

--- Sources/DSSDKCore/DSSDKCore.swift
+++ Sources/DSSDKCore/DSSDKCore.swift
@@ -8,10 +8,3 @@
 @_exported import DSSDK
 
 public enum _DSSDKCore {}  // keep this so SPM still sees at least one Swift file
-
-
-extension DataSapien {
-    @MainActor public static func getIntelligenceService() -> IntelligenceService {
-        return IntelligenceService.shared
-    }
-}
